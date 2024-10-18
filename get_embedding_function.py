b12db39e21d86d493bf8c9432d46b712b534aa72
jaronimas-codes <krambambole@gmail.com>
2024-10-18 13:48:36 +0200

    updated embeddings

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,14 +1,30 @@
+####################################################################################################################
+########################################## OllamaEmbeddings ########################################################
+####################################################################################################################
+
 from langchain_ollama import OllamaEmbeddings
-# from langchain_aws import BedrockEmbeddings
-
-
-
 def get_embedding_function():
     # embeddings = BedrockEmbeddings(
     #     credentials_profile_name="default", region_name="us-east-1"
     # )
     embeddings = OllamaEmbeddings(model="nomic-embed-text")
     return embeddings
+
+#####################################################################################################################
+########################################## BedrockEmbeddings ########################################################
+#####################################################################################################################
+
+# from langchain_aws import BedrockEmbeddings
+
+# def get_embedding_function():
+#     embeddings = BedrockEmbeddings(
+#         credentials_profile_name="default", region_name="us-east-1"
+#     )
+#     return embeddings
+
+#####################################################################################################################
+########################################## OpenAIEmbeddings #########################################################
+#####################################################################################################################
 
 # import openai
 # from dotenv import load_dotenv
